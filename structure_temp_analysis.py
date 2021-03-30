0f9da8290ff9259c931d78677321ced5eb9f5b64
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-03-30 14:37:11 +0530

    Update structure_temp_analysis.py

--- structure_temp_analysis.py
+++ structure_temp_analysis.py
@@ -831,7 +831,7 @@ class StructureFeatureHelper(BaseFeatureHelper):
         return feature_names
 
     def get_features_array(self, prop_graphs, micro_features, macro_features, news_source=None, label=None,
-                           file_dir="data/features", use_cache=False):
+                           file_dir="/content/FakeNewsPropagation/data/features", use_cache=False):
         all_features = []
 
         file_name = self.get_dump_file_name(news_source, micro_features, macro_features, label, file_dir)
